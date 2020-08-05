6631ea145bb02b73e45bd49d3538e42a34a24c0e
ratuku <josuensumba@gmail.com>
2020-08-05 07:18:52 +0200

    first commit

---
+++ src/main/java/com/example/Amigoscode/Spring/Security/AmigoscodeSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.Amigoscode.Spring.Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigoscodeSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigoscodeSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/Amigoscode/Spring/Security/student/Student.java
@@ -0,0 +1,22 @@
+package com.example.Amigoscode.Spring.Security.student;
+
+public class Student {
+
+    private final Integer studentId;
+    private final String studentName;
+
+
+    public Student(Integer studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+}
