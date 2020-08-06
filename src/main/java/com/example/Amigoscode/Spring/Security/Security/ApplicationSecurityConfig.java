8234d11136ec394585998c8d0be87a81be001602
ratuku <josuensumba@gmail.com>
2020-08-06 06:32:12 +0200

    added in memory user and encoding

--- src/main/java/com/example/Amigoscode/Spring/Security/Security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/Amigoscode/Spring/Security/Security/ApplicationSecurityConfig.java
@@ -1,13 +1,31 @@
 package com.example.Amigoscode.Spring.Security.Security;
 
+import com.sun.source.tree.ReturnTree;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import javax.print.DocFlavor;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -18,5 +36,19 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails annaUser = User.builder()
+                .username("Anna")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT") //ROLE_STUDENT
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                annaUser
+        );
     }
 }
