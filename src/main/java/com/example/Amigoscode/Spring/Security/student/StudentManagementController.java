d21be0850f0aa960e48ff3926717bce7de0e6db8
ratuku <josuensumba@gmail.com>
2020-08-06 08:05:31 +0200

    added student management update, delete, register student api

--- src/main/java/com/example/Amigoscode/Spring/Security/student/StudentManagementController.java
+++ src/main/java/com/example/Amigoscode/Spring/Security/student/StudentManagementController.java
@@ -1,19 +1,40 @@
 package com.example.Amigoscode.Spring.Security.student;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/students")
+@RequestMapping("management/api/v1/students")
 public class StudentManagementController {
+
 
     private static final List<Student> STUDENTS = Arrays.asList(
             new Student(1, "James Bond"),
             new Student(2, "Maria Jones"),
             new Student(3, "Anna Smith")
     );
+
+    @GetMapping
+    public List<Student> getAllStudents(){
+        return STUDENTS;
+    }
+
+    @PostMapping
+    public void registerNewStudent(Student student) {
+        System.out.println(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteString(@PathVariable("studentId") Integer studentId){
+        System.out.println(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateString(@PathVariable("studentId")  Integer studentId,
+                             @RequestBody Student student) {
+        System.out.println(String.format("%s %S, studentId, student"));
+    }
 
 }
