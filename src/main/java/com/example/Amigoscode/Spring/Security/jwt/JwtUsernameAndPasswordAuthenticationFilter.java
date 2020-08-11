499bf6c4b981e41ebbc0a7d07d0ec01af2272f95
ratuku <josuensumba@gmail.com>
2020-08-11 19:26:27 +0200

    added jwTCondig days to JwtUsernameAndPasswordAuthenticationFilter

--- src/main/java/com/example/Amigoscode/Spring/Security/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/example/Amigoscode/Spring/Security/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -62,7 +62,7 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
                 .setSubject(authResult.getName())
                 .claim("authorities", authResult.getAuthorities())
                 .setIssuedAt(new Date())
-                .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusWeeks(2)))
+                .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusDays(jwtConfig.getTokenExpirationAfterDays())))
                 .signWith(secretKey)
                 .compact();
         response.addHeader(jwtConfig.getAuthorizationHeader(), jwtConfig.getTokenPrefix() + token);
